e18835a008c33496c7f8055b1aa0d82b1518c23f
kshitiz-goel07 <kshitiz123goel@gmail.com>
2023-07-09 12:52:03 +0530

    Added file that can be used as DB

---
+++ toDoAppFile.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/todos', (req,res)=>{
+    fs.readFile("list.json","utf-8",(err,data)=>{
+        if(err){
+            return err;
+        }
+        else{
+            var result = JSON.parse(data);
+        return res.json(result);
+        }
+    })
+});
+
+app.post("/todos",(req,res)=>{
+    const newToDo = {
+        id : Math.floor(Math.random()*1000),
+        title : req.body.title,
+        description : req.body.description
+    }
+    fs.readFile("list.json","utf-8",(err,data)=>{
+        if(err){
+            return err;
+        }
+        else{
+            const toDo = JSON.parse(data);
+            toDo.push(newToDo);
+            fs.writeFile("list.json",JSON.stringify(toDo) , (err)=>{
+                if(err){
+                    return err;
+                }
+                else{
+                    res.status(201).json(newToDo);
+                }
+            })
+        }
+    })
+})
+
+app.listen(3000);
+
+// let toDo = [];
+
+// app.get("/todos", (req,res)=>{
+//   console.log("your toDO is ", toDo);
+//   return res.json(toDo);
+// })
+
+// app.post("/todos",(req,res)=>{
+//   const newToDo = {
+//     id: Math.floor(Math.random() *10000),
+//     title: req.body.title,
+//     description: req.body.description
+//   }
+//   toDo.push(newToDo);
+//   console.log("ToDo is ", toDo);
+
+//   return res.status(200).send();
+// } )
+
+// app.delete("/todos/:id", (req,res)=>{
+// const index = toDo.findIndex(i => i.id === parseInt(req.params.id));
+// if(index === -1){
+//   return res.status(404).send();
+// }
+// else{
+//   toDo.splice(index,1);
+//   console.log("your toDO is ", toDo);
+//   return res.status(200).send();
+// }
+// })
+
+// app.listen(3000);
+
+
+// module.exports = app;
